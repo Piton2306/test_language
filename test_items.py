382b65ddb2854a9a37e89f470db4f153bb382074
Nastenko Peter <source-306@mail.ru>
2021-12-16 19:04:58 +0300

    fixup! Проверка наличия кнопки "добавить в корзину" на разных языках

--- test_items.py
+++ test_items.py
@@ -2,12 +2,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_language_browser_link_button(browser, execution_number):
+def test_language_browser_link_button(browser):
     browser.get(link)
     button = WebDriverWait(browser, 5).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-primary"))
-    )
-    time.sleep(30)
+    ).text
+    text_result = ['Добавить в корзину', 'Ajouter au panier', 'Añadir al carrito']
+    assert button in text_result, f"Кнопки <{button}> нет!"

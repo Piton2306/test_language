cd61de8428719ac676ca3b627248c5fdcd7d72ac
Nastenko Peter <source-306@mail.ru>
2021-12-16 20:39:10 +0300

    Убрал ожидание элемента

--- test_items.py
+++ test_items.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -8,8 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_language_browser_link_button(browser):
     browser.get(link)
-    button = WebDriverWait(browser, 5).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-primary"))
-    ).text
-    text_result = ['Добавить в корзину', 'Ajouter au panier', 'Añadir al carrito']
-    assert button in text_result, f"Кнопки <{button}> нет!"
+    button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    assert button, "Кнопки 'добавить в корзину' нет!"
+    # time.sleep(30)
